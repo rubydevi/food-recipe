ed5ff37c063537b2799287a4f0efb6b931401717
kit0-0 <kt1767@gmail.com>
2023-10-16 22:52:07 +0530

    Add controller for shoping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,33 @@
 class ShoppingListsController < ApplicationController
+  def index
+    recipe_id = params[:recipe_id]
+    return unless recipe_id.present?
+
+    # Fetch Recipe
+    @recipe = Recipe.find_by(id: recipe_id)
+    return redirect_to root_path, alert: 'Recipe not found' unless @recipe
+
+    # Fetch User Foods
+    @foods = current_user.foods
+
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+
+    @items_to_buy = 0
+    @needed_money = 0
+
+    @recipe_foods.each do |recipe_food|
+      food = recipe_food.food
+      if recipe_food.quantity < food.quantity
+        @items_to_buy += food.quantity - recipe_food.quantity
+        @needed_money += food.price * @items_to_buy
+      end
+    end
+  end
+
+  def generate
+    recipe_id = params[:recipe_id]
+    return redirect_to root_path, alert: 'Recipe not found' unless Recipe.exists?(recipe_id)
+
+    redirect_to shopping_lists_path(recipe_id:)
+  end
 end
