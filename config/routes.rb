322bd8344ed1b6c9fd8ae71052c726358f6cfc10
iamrubydevi <ariesabyss@gmail.com>
2023-10-16 18:07:28 +0530

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'foods#index'
+  root to: 'home#index'
+
+  resources :recipes do
+    resources :recipes_foods, only: [:new, :create, :destroy]
+  end
 
   resources :foods, only: [:index, :show, :new, :create, :destroy]
+
+  # get routes
+  get 'shopping_list', to: 'shopping_lists#show'
+  get 'public_recipes', to: 'recipes#public_recipes'
 end
