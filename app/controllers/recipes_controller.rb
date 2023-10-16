70ce09d0711e6a7ad4e4f97bb88c29c86650666e
kit0-0 <kt1767@gmail.com>
2023-10-16 18:41:46 +0530

    Add controller for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,61 @@
 class RecipesController < ApplicationController
-end
+    before_action :authenticate_user!
+  
+    def index
+      @recipes = current_user.recipes
+    end
+  
+    def show
+      @recipe = Recipe.find(params[:id])
+      @recipe_foods = @recipe.recipe_foods.includes(:food)
+      @preparation_time_hours = @recipe.preparation_time_hours
+      @preparation_time_minutes = @recipe.preparation_time_minutes
+      @cooking_time_hours = @recipe.cooking_time_hours
+      @cooking_time_minutes = @recipe.cooking_time_minutes
+    end
+  
+    def toggle_public
+      @recipe = Recipe.find(params[:id])
+      if @recipe.update(public: !@recipe.public)
+        render json: { public: @recipe.public }
+      else
+        render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def new
+      @recipe = Recipe.new
+    end
+  
+    def create
+      @recipe = Recipe.new(recipe_params)
+      @recipe.user = current_user
+      @recipe.preparation_time = calculate_minutes(params[:recipe][:preparation_time_hr],
+                                                   params[:recipe][:preparation_time_min])
+      @recipe.cooking_time = calculate_minutes(params[:recipe][:cooking_time_hr], params[:recipe][:cooking_time_min])
+  
+      if @recipe.save
+        redirect_to @recipe, notice: 'Recipe successfully created.'
+      else
+        render 'new', alert: 'Failed to create a new recipe.'
+      end
+    end
+  
+    def destroy
+      @recipe = Recipe.find(params[:id])
+      @recipe.destroy!
+  
+      redirect_to recipes_path, notice: 'Recipe successfully deleted.'
+    end
+  
+    private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :description, :public)
+    end
+  
+    def calculate_minutes(hours, minutes)
+      (hours.to_i * 60) + minutes.to_i
+    end
+  end
+  
